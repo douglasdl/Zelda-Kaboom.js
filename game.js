a0ab777ad996e148ae4d89447ee595fd9e4abc40
Douglas Dias Leal <douglas_san@hotmail.com>
2022-04-21 13:01:57 +0900

    Fix overlaps() bug changing by collides()

--- game.js
+++ game.js
@@ -36,52 +36,77 @@ loadSprite('grass', 'grass.png')
 loadSprite('hole', 'hole.png')
 loadSprite('arrow', 'arrow.png')
 
-const map = [
-    'xccmcc^ccw',
-    'a        b',
-    'a      * b',
-    'a    f   b',
-    '<        b',
-    'a    f   b',
-    'a   *    b',
-    'a        b',
-    'zddmddmddy'
+const maps = [
+    [
+        'xccmcc^ccw',
+        'a        b',
+        'a      * b',
+        'a    f   b',
+        '<        b',
+        'a    f   b',
+        'a   *    b',
+        'a        b',
+        'zddmddmddy'
+    ],
+    [
+        'xccccccccw',
+        'a        b',
+        'm        m',
+        'a        b',
+        'a        b',
+        'a    s   b',
+        'm  }     m',
+        'a        b',
+        'zddddddddy'
+    ]
 ]
 
 scene("game", ({ level, score }) => {
 
     layers(['bg', 'obj', 'ui'], 'obj')
 
-    const gameLevel = addLevel(map, {
+    const gameLevel = addLevel(maps[level], {
         width: 48,
         height: 48,
         pos: vec2(50, 250),
-        'a': () => [sprite('left-wall'), area(), solid(), origin("bot")],
-        'b': () => [sprite('right-wall'), area(), solid(), origin("bot")],
-        'c': () => [sprite('top-wall'), area(), solid(), origin("bot")],
-        'd': () => [sprite('bottom-wall'), area(), solid(), origin("bot")],
-        'w': () => [sprite('top-right-wall'), area(), solid(), origin("bot")],
-        'x': () => [sprite('top-left-wall'), area(), solid(), origin("bot")],
-        'y': () => [sprite('bottom-right-wall'), area(), solid(), origin("bot")],
-        'z': () => [sprite('bottom-left-wall'), area(), solid(), origin("bot")],
-        '^': () => [sprite('top-door'), "next-level", area(), origin("bot")],
-        '<': () => [sprite('left-door'), "next-level", area(), solid(), origin("bot")],
+        'a': () => [sprite('left-wall'), "wall", area(), solid(), origin("bot")],
+        'b': () => [sprite('right-wall'), "wall", area(), solid(), origin("bot")],
+        'c': () => [sprite('top-wall'), "wall", area(), solid(), origin("bot")],
+        'd': () => [sprite('bottom-wall'), "wall", area(), solid(), origin("bot")],
+        'w': () => [sprite('top-right-wall'), "wall", area(), solid(), origin("bot")],
+        'x': () => [sprite('top-left-wall'), "wall", area(), solid(), origin("bot")],
+        'y': () => [sprite('bottom-right-wall'), "wall", area(), solid(), origin("bot")],
+        'z': () => [sprite('bottom-left-wall'), "wall", area(), solid(), origin("bot")],
+        '^': () => [sprite('top-door'), "next-level", "door", area(), origin("bot")],
+        '<': () => [sprite('left-door'), "next-level", "door", area(), solid(), origin("bot")],
         's': () => [sprite('stairs'), "next-level", area(), origin("bot")],
-        '*': () => [sprite('slicer'), area(), origin("bot")],
-        '}': () => [sprite('skeletor'), area(), origin("bot")],
+        '*': () => [sprite('slicer'), "dangerours", "slicer", area(), { dir: -1 }, origin("bot")],
+        '}': () => [sprite('skeletor'), "dangerours", "skeletor", area(), { dir: -1 }, origin("bot")],
         'm': () => [sprite('lanterns'), area(), solid(), origin("bot")],
         'f': () => [sprite('fire-pot'), area(), solid(), origin("bot")],
     })
 
     //add(sprite('floor'), area(gameLevel.width, gameLevel.height), solid(), origin("bot"))
 
-    add([text(score), pos(100, 150), layer('ui'), scale(0.5)])
+    const scoreLabel = add([
+        text('0'), 
+        pos(100, 150), 
+        layer('ui'),
+        {
+            value: score,
+        },
+        scale(0.5)
+    ])
 
     add([text('Level ' + level), pos(200, 150), layer('ui'), scale(0.5)])
 
-    const player = add([sprite('link-going-right'), pos(70, 450), layer('obj'),
+    const player = add([
+        sprite('link-going-right'), 
+        area(), 
+        pos(70, 450), 
+        layer('obj'),
         {
-            // Right by default
+            //Right by default
             dir: vec2(1, 0),
         }
     ])
@@ -90,7 +115,12 @@ scene("game", ({ level, score }) => {
         //player.resolve()
     })
 
-    //player.overlaps()
+    player.collides('next-level', () => {
+        go('game', {
+            level: (level + 1) % maps.length,
+            score: scoreLabel.value
+        })
+    })
 
     keyDown('left', () => {
         player.use(sprite('link-going-left'))
@@ -114,6 +144,16 @@ scene("game", ({ level, score }) => {
     })
 
 
+    // Slicer
+    const SLICER_SPEED = 100
+    action('slicer', (s) => {
+        s.move(s.dir * SLICER_SPEED, 0)
+    })
+
+    collides('slicer', 'wall', (s) => {
+        s.dir = -s.dir
+    })
+
 
 
 
@@ -122,7 +162,7 @@ scene("game", ({ level, score }) => {
 function start() {
 	// Start with the "game" scene, with initial parameters
 	go("game", {
-        level: 0,
+        level: 1,
         score: 0,
 	})
 }
